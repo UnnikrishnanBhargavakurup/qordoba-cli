0b1d2d4f6a218c27120edd26d0d754910157c349
Franzi Wittleder <franzi@qordoba.com>
2017-10-11 20:19:14 -0700

    ENG-1406 added replace logix

--- qordoba/commands/i18n_execute.py
+++ qordoba/commands/i18n_execute.py
@@ -21,27 +21,52 @@ class i18nExecutionClass(BaseClass):
         self.next = False
 
     def replace_strings_for_keys(self, df_to_dict, file_array):
+        # Python 2
         try:
             for k, v in df_to_dict.iteritems():
-                for _, array in v.iteritems():
-                    print(array)
+                if v['startLineNumber'] == v['endLineNumber']:
+                    idx = v['startLineNumber']-1
+                    picked_line = file_array[idx]
+                    picked_line = picked_line.replace("'" + v['text']+"'", "${" + v['generated_keys'] + "}")
+                    picked_line = picked_line.replace('"' + v['text'] + '"', "${" + v['generated_keys'] + "}")
+                    file_array[idx] = picked_line
+                if v['startLineNumber'] < v['endLineNumber']:
+                    idx_start = v['startLineNumber'] - 1
+                    idx_end = v['endLineNumber'] - 1
+                    picked_lines = file_array[idx_start:idx_end]
+                    print('START')
+                    print(picked_lines)
+                    print(v['startLineNumber'], v['endLineNumber'])
 
-                '''{'
-                filename': '/work_dir/./folder/cloudflare-json-translation-example.json', 
-                'startLineNumber': 13, 
-                'startCharIdx': 30, 
-                'endLineNumber': 13, 
-                'endCharIdx': 33, 
-                'text': '21', 
-                'existing_eys': None, 
-                'existing_localization_file': None, 
-                'generated_keys': '21'}'''
+
+        # Python 3
         except AttributeError:
             for k, v in df_to_dict.items():
-                print(v)
-                print(v['startLineNumber'])
-                # for _, array in v.items():
-                #     print(type(array))
+
+                if v['startLineNumber'] == v['endLineNumber']:
+                    idx = v['startLineNumber']-1
+                    picked_line = file_array[idx]
+                    picked_line = picked_line.replace("'" + v['text']+"'", "${" + v['generated_keys'] + "}")
+                    picked_line = picked_line.replace('"' + v['text'] + '"', "${" + v['generated_keys'] + "}")
+                    file_array[idx] = picked_line
+
+                if v['startLineNumber'] < v['endLineNumber']:
+                    idx_start = v['startLineNumber'] - 1
+                    idx_end = v['endLineNumber'] - 1
+                    picked_lines = file_array[idx_start:idx_end]
+                    joined_lines = '\n'.join(picked_lines)
+                    print('START')
+                    print(joined_lines)
+                    print(picked_lines)
+                    print(v['startLineNumber'], v['endLineNumber'])
+                    print(v['text'])
+                    picked_line = picked_lines.replace("'" + v['text'] + "'", "${" + v['generated_keys'] + "}")
+                    picked_line = picked_lines.replace('"' + v['text'] + '"', "${" + v['generated_keys'] + "}")
+                    file_array[idx_end] = picked_line
+                    for i in range(idx_start, idx_end):
+                        file_array[i] = None
+
+                return file_array
 
 
 
@@ -73,11 +98,18 @@ class i18nExecutionClass(BaseClass):
                 df_single_file = df[df.filename == file_in_report]
                 df_to_dict = df_single_file.T.to_dict()
                 file_array = self.get_filerows_as_list(project_file_path)
-                new_file = self.replace_strings_for_keys(df_to_dict, file_array)
+                print(file_array)
+                new_file_array = self.replace_strings_for_keys(df_to_dict, file_array)
+                new_file_array = [x for x in new_file_array if x != None]
 
                 # remove old file, dump new
-                # os.remove(file_path)
-                # for item in new_file:
-                #     file_path.write("%s\n" % item)
+                # os.remove(project_file_path)
+                project_file_path_new = directory + '/new_w_keys.html'
+                Html_file = open(project_file_path_new, "w")
+                Html_file.write("\n".join(new_file_array))
+                Html_file.close()
+                # with open(project_file_path, "w") as new_file:
+                #     new_file.write("\n".join(new_file_array))
+
 
         #create localization file in output folder
