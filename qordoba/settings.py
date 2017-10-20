9b486488134e5e3c0c7c7f7c78542cc0fd05f036
Franzi Wittleder <franzi@qordoba.com>
2017-10-20 11:55:25 -0700

    setting.py

--- qordoba/settings.py
+++ qordoba/settings.py
@@ -10,9 +10,9 @@ log = logging.getLogger('qordoba')
 DEFAULT_SETTING_PATH = os.path.abspath(os.path.join(os.getcwd(), 'config.yml'))
 
 SETTING_PATHS = (
-    os.environ.get('QORDOBA_CONFIG', ''),
+    # os.environ.get('QORDOBA_CONFIG', ''),
     DEFAULT_SETTING_PATH,
-    os.path.abspath(os.path.join(os.path.expanduser('~'), '.qordoba.yml'))
+    # os.path.abspath(os.path.join(os.path.expanduser('~'), '.qordoba.yml'))
 )
 
 
@@ -42,15 +42,15 @@ class SettingsDict(dict):
         if validate:
             self.validate()
 
-    # def validate(self, keys=('project_id', 'access_token')):
-    #     for key in keys:
-    #         try:
-    #             v = self[key]
-    #             if v is None:
-    #                 raise KeyError
-    #         except KeyError:
-    #             raise SettingsValidationError(
-    #                 """{} param is required. Please provide it by argument or in config file.""".format(key))
+    def validate(self, keys=('project_id', 'access_token')):
+        for key in keys:
+            try:
+                v = self[key]
+                if v is None:
+                    raise KeyError
+            except KeyError:
+                raise SettingsValidationError(
+                    """{} param is required. Please provide it by argument or in config file.""".format(key))
 
 
 def load_settings_from_file(path):
